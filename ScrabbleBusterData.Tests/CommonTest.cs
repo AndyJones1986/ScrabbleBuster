dc22ae6f0c594a792e239f64ade8826046c9ab46
Andrew Jones <Andrew.F.Jones@outlook.com>
2019-01-15 13:49:41 +0000

    Commiting changes, added litedb instantiate

---
+++ ScrabbleBusterData.Tests/CommonTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ScrabbleBusterData;
+
+namespace ScrabbleBusterData.Tests
+{
+    [TestClass]
+    public class CommonTest
+    {
+        [TestMethod]
+        public void InsantiateDBTest()
+        {
+            using (Common dbCommon = new Common("TESTDB"))
+            {
+                Assert.IsNotNull(Common.Database);
+            };
+
+            Assert.IsNull(Common.Database);
+        }
+    }
+}
---
+++ ScrabbleBusterData/Common.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LiteDB;
+
+namespace ScrabbleBusterData
+{
+    public class Common : IDisposable
+    {
+        public static LiteDatabase Database { get; set; }
+        public Common(string databaseName)
+        {
+            Database = new LiteDatabase(databaseName);
+        }
+
+        public void Dispose()
+        {
+            Database.Dispose();
+            Database = null;
+        }
+    }
+}
